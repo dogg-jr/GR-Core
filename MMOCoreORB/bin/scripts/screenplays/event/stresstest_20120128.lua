6f7e1ea7fec18b4d42051dfe06537c57de6293a3
ObiGr <ObiGr@outlook.com>
2014-07-25 12:07:04 -0700

    {Changed stresstest}
    {Changed stresstest}

--- MMOCoreORB/bin/scripts/screenplays/event/stresstest_20120128.lua
+++ MMOCoreORB/bin/scripts/screenplays/event/stresstest_20120128.lua
@@ -58,7 +58,7 @@ function stresstest_20120128:spawnSceneObjects()
 	collector2:setCustomObjectName("\\#33ee77Travel to the Geonosian Bio Lab")
 	createObserver(OBJECTRADIALUSED, "stresstest_20120128", "teleportGEO", pCollector2)
 	
-	--Return from DWB
+	Return from DWB
 	local pCollector3 = spawnSceneObject("endor", "object/tangible/furniture/imperial/data_terminal_s1.iff", -4660.2, 12.8, 4286 , 0, 0.707107, 0, 0.707107, 0)
 	local collector3 = LuaSceneObject(pCollector3)
 	local col3creo = LuaCreatureObject(pCollector3)
@@ -66,13 +66,13 @@ function stresstest_20120128:spawnSceneObjects()
 	collector3:setCustomObjectName("\\#ee3377Travel to Coronet, Corellia")
 	createObserver(OBJECTRADIALUSED, "stresstest_20120128", "teleportCnet", pCollector3)
 	
-	--DWB BLUEFROG
+	DWB BLUEFROG
 	spawnSceneObject("endor", "object/tangible/terminal/terminal_character_builder.iff", -4654.5, 14.6, 4332.6, 0, 0, 0, 1, 0)
 	
-	--GEO BLUEFROG
+	GEO BLUEFROG
 	spawnSceneObject("yavin4", "object/tangible/terminal/terminal_character_builder.iff", -6479.1, 84.2, -435.3, 0, 0.4771588, 0, -0.8788171, 0)
 	
-	--Return from GEO
+	Return from GEO
 	local pCollector4 = spawnSceneObject("yavin4", "object/tangible/furniture/imperial/data_terminal_s1.iff", -6493.1, 84.2, -452.3, 0, 0.996917, 0, 0.078459, 0)
 	local collector4 = LuaSceneObject(pCollector4)
 	local col4creo = LuaCreatureObject(pCollector4)
